d6daa325face02da7015b3cbe063dbfa45809803
Tahir <dibirovtahir@gmail.com>
2022-12-10 13:53:13 +0400

    #

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const compNameInput = document.querySelector('.company-name')
 const contNameInput = document.querySelector('.contact-name')
 const contTitleInput = document.querySelector('.contact-title')
 const submitButton = document.querySelector('.submit')
+const table = document.querySelector('table')
 
 const data = {}
+
+window.addEventListener('load', ()=>{
+    getTable(URL, root)
+})
 
 compNameInput.addEventListener('input', (event)=>{
     data.companyName = event.target.value
@@ -27,7 +32,6 @@ const getTable = async (URL, mountPoint) =>{
 
     const response  = await fetch(URL)
     const data = await response.json()
-    const table = document.createElement('table')
     
     data.forEach(element => {
         const tr = document.createElement('tr')
@@ -39,12 +43,27 @@ const getTable = async (URL, mountPoint) =>{
         const tdContTitle = document.createElement('td')
         tdContTitle.innerHTML = element.contactTitle
         const removeButton = document.createElement('button')
+        removeButton.classList.add('remove')
         removeButton.textContent = "remove"
         tr.append(tdCompName)
         tr.append(tdContName)
         tr.append(tdContTitle)
         tr.append(removeButton)
         table.appendChild(tr)
+
+
+      
+            removeButton.addEventListener('click', async (event)=>{
+               document.querySelector('table').innerHTML = ''
+                let id = event.target.parentNode.getAttribute('key')
+               await deletePOST(URL, id)
+               
+            })
+            
+   
+        
+
+
     });
 
     mountPoint.append(table)
@@ -66,16 +85,27 @@ const createPost = async (URL, data)=>{
 }
 
 
-const editPost = async (URL, data, id)=>{
-    await fetch(`${URL}/${id}`, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          method: "PUT",
-          body: JSON.stringify(data)
-    })
+// const editPost = async (URL, data, id)=>{
+//     await fetch(`${URL}/${id}`, {
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//           },
+//           method: "PUT",
+//           body: JSON.stringify(data)
+//     })
 
+    
+//     getTable(URL, root)
+// }
+
+
+
+
+const deletePOST = async (URL, id)=>{
+    await fetch(`${URL}/${id}`, {
+        method: 'DELETE'
+    })
     document.querySelector('table').innerHTML = ''
     getTable(URL, root)
 }
@@ -84,24 +114,25 @@ const editPost = async (URL, data, id)=>{
 
 
 
-const edit = async()=>{
-   await getTable(URL, root)
+
     document.querySelectorAll('tr')
 .forEach(tr=> {
     tr.addEventListener('click', (event)=>{
        compNameInput.value = tr.querySelectorAll('td')[0].textContent
        contNameInput.value = tr.querySelectorAll('td')[1].textContent
        contTitleInput.value = tr.querySelectorAll('td')[2].textContent
-
+        console.log(tr)
        console.log(event.target.key)
        submitButton.addEventListener('click', ()=>{
-        editPost(URL, {companyName: compNameInput, contNameInput: contNameInput.value, contactTitle: contTitleInput.value}, event.target.key)
+        document.querySelector('table').innerHTML = ''
+        editPost(URL, {companyName: compNameInput.value, contNameInput: contNameInput.value, contactTitle: contTitleInput.value}, event.target.key)
        })
             
     })
 })
-}
 
-edit()
 
-getTable(URL, root)
+
+
+
+
